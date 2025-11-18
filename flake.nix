a9379d80f578edc8b04f37afc366b5d5057f506b
imlyzh <enterindex@gmail.com>
2025-11-19 07:49:52 +0800

    feat: pin home-manager to release-25.05 branch
    Update home-manager input URL to use the stable release-25.05 branch instead of the default branch. This ensures reproducible builds and prevents unexpected changes from the unstable default branch.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # rust-overlay = {
