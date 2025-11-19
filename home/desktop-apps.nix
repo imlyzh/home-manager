602a3cc128ab01df0a41794c4ae0355884421e85
imlyzh <enterindex@gmail.com>
2025-11-20 03:03:59 +0800

    feat: split desktop apps configuration into separate files
    Refactor desktop applications configuration by splitting into two files:
    - desktop-apps.nix now contains cross-platform applications
    - linux-desktop-apps.nix contains Linux-specific desktop applications
    
    This improves maintainability and organization by separating platform-specific
    configurations, making it easier to manage different desktop environments
    and potentially support other platforms in the future.

--- home/desktop-apps.nix
+++ home/desktop-apps.nix
@@ -3,67 +3,16 @@
 {
   home.packages = with pkgs; [
     noto-fonts-cjk-sans # 中日韩字体，防止乱码
-    font-awesome # 好看的图标字体，waybar会用到
 
-    kitty
-    ghostty
-
-    fuzzel
-
-    #fcitx5
-    ibus
-
-    clash-verge-rev
-    vscode
-    file-roller
-
-    firefox
     firefox-devedition
     spotify
     telegram-desktop
     mumble
     syncplay
-    vlc
     mpv
 
     localsend
-    motrix
     qbittorrent-enhanced
     transmission_4-gtk
-
-    anki
-
-    # podman-desktop
-
-    # discord
-    # obsidian
-    # logseq
-
-    # teamspeak3
-    # teamspeak_client
-    # teamspeak_server
-    # zoom-us
-
-    # steam
-
   ];
-
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "FiraCode Nerd Font";
-      size = 14;
-    };
-  };
-  programs.ghostty = {
-    enable = true;
-  };
-
-  programs.firefox.enable = true;
-
-  # xdg.configFile = {
-  #   "./.config/assets".text = import ../dotfiles/.config/assets;
-  #   "./.config/waybar".text = import ../dotfiles/.config/waybar;
-  #   "./.config/niri/config.kdl".text = import .../dotfiles/.config/niri/config.kdl;
-  # };
 }
---
+++ home/linux-desktop-apps.nix
@@ -0,0 +1,59 @@
+# 你可以把这个文件保存为 desktop.nix，然后在你的主 home.nix 中导入它
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    font-awesome # 好看的图标字体，waybar会用到
+
+    kitty
+    ghostty
+
+    fuzzel
+
+    #fcitx5
+    ibus
+
+    file-roller
+
+    clash-verge-rev
+    vscode
+
+    firefox
+    vlc
+
+    motrix
+
+    anki
+
+    # podman-desktop
+
+    # discord
+    # obsidian
+    # logseq
+
+    # teamspeak3
+    # teamspeak_client
+    # teamspeak_server
+    # zoom-us
+
+    # steam
+  ];
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 12;
+    };
+  };
+  programs.ghostty = {
+    enable = true;
+  };
+
+  programs.firefox.enable = true;
+
+  # xdg.configFile = {
+  #   "./.config/assets".text = import ../dotfiles/.config/assets;
+  #   "./.config/waybar".text = import ../dotfiles/.config/waybar;
+  #   "./.config/niri/config.kdl".text = import .../dotfiles/.config/niri/config.kdl;
+  # };
+}
