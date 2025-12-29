ba5ba455104b5c713f4bd1616cc3bb9e8c554a47
imlyzh <enterindex@gmail.com>
2025-12-30 01:25:08 +0800

    feat(rime): update dotfiles rev and switch to custom pinyin schemas
    - Update dotfiles flake lock to latest revision (d5b6147)
    - Replace flypy_ice.schema.yaml with double_pinyin.custom.yaml
    - Replace double_pinyin_zrm_xing.schema.yaml with double_pinyin_flypy.custom.yaml
    - This change switches from specific schema files to more flexible custom configuration files for double pinyin input methods

--- home/rime.nix
+++ home/rime.nix
@@ -12,6 +12,6 @@ in
 
   home.file."${rimeDir}/default.custom.yaml".source = "${dotfiles}/rime/default.custom.yaml";
   home.file."${rimeDir}/squirrel.custom.yaml".source = "${dotfiles}/rime/squirrel.custom.yaml";
-  home.file."${rimeDir}/flypy_ice.schema.yaml".source = "${dotfiles}/rime/flypy_ice.schema.yaml";
-  home.file."${rimeDir}/double_pinyin_zrm_xing.schema.yaml".source = "${dotfiles}/rime/double_pinyin_zrm_xing.schema.yaml";
+  home.file."${rimeDir}/double_pinyin.custom.yaml".source = "${dotfiles}/rime/double_pinyin.custom.yaml";
+  home.file."${rimeDir}/double_pinyin_flypy.custom.yaml".source = "${dotfiles}/rime/double_pinyin_flypy.custom.yaml";
 }
